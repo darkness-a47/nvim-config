97845abf82ef2507505aa985f777032414ef4e42
darkness <mahan1122mg@gmail.com>
2022-11-19 01:39:25 +0330

    add init.lua

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("lua/init")
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.compatible = false
+vim.opt.filetype = 'on'
+vim.opt.syntax = 'on'
+vim.opt.backup = false
+vim.opt.hlsearch = true
+vim.opt.showcwd = true
+vim.opt.showmode = true
+vim.opt.showmatch = true
